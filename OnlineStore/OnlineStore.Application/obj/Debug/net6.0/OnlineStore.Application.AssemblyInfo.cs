a12dbb2fc75bcf990358262c82e1aca9d54c796d
abolfazl pirhadi <dev.abolfazlpirhadi@gmail.com>
2024-08-26 20:02:51 +0330

    .

--- OnlineStore/OnlineStore.Application/obj/Debug/net6.0/OnlineStore.Application.AssemblyInfo.cs
+++ OnlineStore/OnlineStore.Application/obj/Debug/net6.0/OnlineStore.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97fe6dc174b497b1d118ee065ad122109c4a31e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c45f6b55004a042657c82bf1d39122caf4585e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
